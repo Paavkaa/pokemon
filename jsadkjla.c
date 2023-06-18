6a2b8a395fb033ec8226799991c75e64172ed0c2
Paavkaa <terezapavelkova1@gmail.com>
2023-06-18 10:29:41 +0200

    Změna názvu na náhodný, protože původní soubor nebylo možné zkompilovat. Finální úprava kódu

--- jsadkjla.c
+++ jsadkjla.c
@@ -23,13 +23,13 @@ int grass_attack(int choice);
 int electric_attack(int choice);
 
 int damage(int choice, Pokemon on_turn, Pokemon on_defense);
-int fight(Pokemon player, Pokemon enemy);
 
 int main()
 {
     srand(time(NULL));
     //volba pokémona
-    int choice;
+    int choice, attack, round = 1;
+
     printf("Vyber si pokemona:\n");
 
     printf("1. Charmander (fire)\n");
@@ -42,27 +42,29 @@ int main()
     Pokemon player;
     Pokemon enemy;
 
+    if (choice < 1 || choice > 4)
+    {
+        printf("Neplatná volba, bude pridelen nahodny pokemon\n");
+        choice = rand() % 4 + 1;
+    }
+
     if (choice == 1)
     {
-        player =  (Pokemon) {"Charmander", 65, 10, 40, "fire", "Scratch", "Ember", "Fire Fang"};
+        player =  (Pokemon) {"Charmander", 65, 5, 40, "fire", "Scratch", "Ember", "Fire Fang"};
     }
     else if (choice == 2)
     {
-        player = (Pokemon) {"Squirtle", 100, 12, 20, "water", "Tackle", "Water Gun", "Bubble Beam"};
+        player = (Pokemon) {"Squirtle", 100, 6, 20, "water", "Tackle", "Water Gun", "Bubble Beam"};
     }
     else if (choice == 3)
     {
-        player = (Pokemon) {"Bulbasaur", 70, 35, 80, "grass", "Tackle", "Vine Whip", "Razor Leaf"};
+        player = (Pokemon) {"Bulbasaur", 70, 12, 80, "grass", "Tackle", "Vine Whip", "Razor Leaf"};
     }
     else if (choice == 4)
     {
-        player = (Pokemon) {"Pikachu", 95, 20, 90, "electric", "Quick Attack", "Thunder Shock", "Thunderbolt"};
+        player = (Pokemon) {"Pikachu", 95, 10, 90, "electric", "Quick Attack", "Thunder Shock", "Thunderbolt"};
     }
-    else
-    {
-        printf("Neplatná volba\n");
-        return 0;
-    }
+
 
     //volba soupeře
     choice = rand() % 4 + 1;
@@ -83,29 +85,131 @@ int main()
         enemy = (Pokemon) {"Pikachu", 95, 10, 90, "electric", "Quick Attack", "Thunder Shock", "Thunderbolt"}; //? electric - rychlý, střední útok a obrana, vysoké HP
     }
 
-    fight(player, enemy);
+    system("cls");
 
-    return 0;
+    printf(" %s ( %s) vs %s ( %s)", player.name, player.type, enemy.name, enemy.type);
+    if (player.speed >= enemy.speed)
+    {
+        printf("\nZacina hrac");
+    }
+    else
+    {
+        printf("\nZacina souper");
+    }
+
+    Sleep(2000);
+    system("cls");
+
+    while (player.hp > 0 && enemy.hp > 0)
+    {
+         if(player.speed >= enemy.speed)
+        {
+            printf("Hrac je na tahu:\n");
+            printf("1. %s\t", player.attack1);
+            printf("2. %s\t", player.attack2);
+            printf("3. %s\n", player.attack3);
+
+            scanf("%d", &choice);
+
+            attack = damage(choice, player, enemy);
+            printf("Zasah: %d\n", attack);
+            enemy.hp -= attack;
+
+            printf("souper je na tahu:\n");
+            choice = rand() % 3 + 1;
+
+            attack = damage(choice, enemy, player);
+            printf("Zasah: %d\n", attack);
+            player.hp -= attack;
+        }
+        else
+        {
+            printf("souper je na tahu:\n");
+            choice = rand() % 3 + 1;
+
+            attack = damage(choice, enemy, player);
+            printf("Zasah: %d\n", attack);
+            player.hp -= attack;
+
+            printf("Hrac je na tahu:\n");
+            printf("1. %s   ", player.attack1);
+            printf("2. %s   ", player.attack2);
+            printf("3. %s\n", player.attack3);
+
+            scanf("%d", &choice);
+
+            attack = damage(choice, player, enemy);
+            printf("Zasah: %d\n", attack);
+            enemy.hp -= attack;
+        }
+
+        Sleep(2000);
+        system("cls");
+
+        printf("Stav %d kola:\n", round);
+        printf("Hrac: %d\n", player.hp);
+        printf("Souper: %d\n", enemy.hp);
+
+        Sleep(2000);
+        system("cls");
+
+        round++;
+    }
+    if (player.hp > 0)
+    {
+        printf("Vyhral jsi");
+    }
+    if (player.hp <= 0 && enemy.hp <= 0)
+    {
+        printf("Remiza");
+    }
 }
 
-int fire_attack(int choice) //? vysoký útok
+int fire_attack(int choice) // Vysoký útok
 {
     int attack;
 
     if (choice == 1)
     {
-        //rozmezí útoku 35-45
+        // Rozmezí útoku 40-50
+        attack = rand() % 11 + 40;
+    }
+    else if (choice == 2)
+    {
+        // Rozmezí útoku 35-45
+        attack = rand() % 11 + 35;
+    }
+    else if (choice == 3)
+    {
+        // Rozmezí útoku 45-55
+        attack = rand() % 11 + 45;
+    }
+    else
+    {
+        attack = 0;
+    }
+
+    return attack;
+}
+
+int water_attack(int choice) // Střední útok
+{
+    int attack;
+
+    if (choice == 1)
+    {
+        // Rozmezí útoku 35-45
         attack = rand() % 11 + 35;
     }
     else if (choice == 2)
     {
-        //rozmezí útoku 20-50
-        attack = rand() % 21 + 30;
+        // Rozmezí útoku 30-40
+        attack = rand() % 11 + 30;
     }
     else if (choice == 3)
     {
-        //rozmezí útoku 38-43
-        attack = rand() % 4 + 40;
+        // Rozmezí útoku 40-50
+        attack = rand() % 11 + 40;
     }
     else
     {
@@ -115,24 +219,24 @@ int fire_attack(int choice) //? vysoký útok
     return attack;
 }
 
-int water_attack(int choice) //? střední útok
+int grass_attack(int choice) // Nízký útok
 {
     int attack;
 
     if (choice == 1)
     {
-        //rozmezí útoku 25-35
+        // Rozmezí útoku 30-40
+        attack = rand() % 11 + 30;
+    }
+    else if (choice == 2)
+    {
+        // Rozmezí útoku 25-35
         attack = rand() % 11 + 25;
     }
-    else if (choice == 2)
-    {
-        //rozmezí útoku 20-40
-        attack = rand() % 21 + 20;
-    }
     else if (choice == 3)
     {
-        //rozmezí útoku 32-38
-        attack = rand() % 7 + 32;
+        // Rozmezí útoku 35-45
+        attack = rand() % 11 + 35;
     }
     else
     {
@@ -142,24 +246,24 @@ int water_attack(int choice) //? střední útok
     return attack;
 }
 
-int grass_attack(int choice) //? nízký útok
+int electric_attack(int choice) // Střední útok
 {
     int attack;
 
     if (choice == 1)
     {
-        //rozmezí útoku 15-25
-        attack = rand() % 11 + 15;
+        // Rozmezí útoku 35-45
+        attack = rand() % 11 + 35;
     }
     else if (choice == 2)
     {
-        //rozmezí útoku 10-30
-        attack = rand() % 21 + 10;
+        // Rozmezí útoku 30-40
+        attack = rand() % 11 + 30;
     }
     else if (choice == 3)
     {
-        //rozmezí útoku 27-32
-        attack = rand() % 6 + 27;
+        // Rozmezí útoku 40-50
+        attack = rand() % 11 + 40;
     }
     else
     {
@@ -169,40 +273,15 @@ int grass_attack(int choice) //? nízký útok
     return attack;
 }
 
-int electric_attack(int choice) //? střední útok
-{
-    int attack;
 
-    if (choice == 1)
-    {
-        //rozmezí útoku 25-35
-        attack = rand() % 11 + 25;
-    }
-    else if (choice == 2)
-    {
-        //rozmezí útoku 20-40
-        attack = rand() % 21 + 20;
-    }
-    else if (choice == 3)
-    {
-        //rozmezí útoku 32-38
-        attack = rand() % 7 + 32;
-    }
-    else
-    {
-        attack = 0;
-    }
-
-    return attack;
-}
 
 int damage(int choice, Pokemon on_turn, Pokemon on_defense)
 {
-    int damage, defense;
+    float attack, defense;
 
     if (strcmp(on_turn.type, "fire") == 0)
     {
-        damage = fire_attack(choice);
+        attack = fire_attack(choice);
 
         if(strcmp(on_defense.type, "fire") == 0)
         {
@@ -223,7 +302,7 @@ int damage(int choice, Pokemon on_turn, Pokemon on_defense)
     }
     else if (strcmp(on_turn.type, "water") == 0)
     {
-        damage = water_attack(choice);
+        attack = water_attack(choice);
 
         if(strcmp(on_defense.type, "fire") == 0)
         {
@@ -245,7 +324,7 @@ int damage(int choice, Pokemon on_turn, Pokemon on_defense)
     }
     else if (strcmp(on_turn.type, "grass") == 0)
     {
-        damage = grass_attack(choice);
+        attack = grass_attack(choice);
 
         if(strcmp(on_defense.type, "fire") == 0)
         {
@@ -266,7 +345,7 @@ int damage(int choice, Pokemon on_turn, Pokemon on_defense)
     }
     else if (strcmp(on_turn.type, "electric") == 0)
     {
-        damage = electric_attack(choice);
+        attack = electric_attack(choice);
 
         if(strcmp(on_defense.type, "fire") == 0)
         {
@@ -287,63 +366,11 @@ int damage(int choice, Pokemon on_turn, Pokemon on_defense)
         }
     }
 
-    damage = damage * defense - on_defense.defense;
-    return damage;
-}
-
-int fight(Pokemon player, Pokemon enemy)
-{
-    int choice, attack, round = 1;
-    printf(" %s ( %s) vs %s ( %s)", player.name, player.type, enemy.name, enemy.type);
-    if (player.speed >= enemy.speed)
+    attack = attack * defense - on_defense.defense;
+    if (attack < 0)
     {
-        printf("Zacina hrac");
+        attack = 0;
     }
-    else
-    {
-        printf("Zacina souper");
-    }
-
-    while (player.hp > 0 && enemy.hp > 0)
-    {
-         if(player.speed >= enemy.speed)
-        {
-            printf("Hrac je na tahu:\n");
-            printf("1. %s\t", player.attack1);
-            printf("2. %s\t", player.attack2);
-            printf("3. %s\n", player.attack3);
-
-            scanf("%d", &choice);
-
-            attack = damage(choice, player, enemy);
-            printf("Zasah: %d\n", attack);
-            enemy.hp -= attack;
-        }
-
-        if(enemy.hp > 0)
-        {
-            printf("souper je na tahu:\n");
-            choice = rand() % 3 + 1;
-
-            attack = damage(choice, enemy, player);
-            printf("Zasah: %d\n", attack);
-            player.hp -= attack;
-        }
-
-        printf("Stav %d kola:\n", round);
-        printf("Hrac: %d\n", player.hp);
-        printf("Souper: %d\n", enemy.hp);
-
-        round++;
-    }
-    if (player.hp > 0)
-    {
-        printf("Vyhral jsi");
-        return 1;
-    }
-    else
-    {
-        printf("Prohral jsi");
-        return 0;
-    }
+    
+    return attack;
 }
